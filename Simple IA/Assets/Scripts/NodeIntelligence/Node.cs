d579c3d4e4e509c251b93a739a3776a403d7a299
Sauron <federicoolive@gmail.com>
2022-10-06 20:44:00 -0300

    Create diferents nodes for AI Tree

---
+++ Simple IA/Assets/Scripts/NodeIntelligence/Node.cs
@@ -0,0 +1,87 @@
+using System.Collections.Generic;
+
+namespace Intelligence
+{
+    public enum NodeState
+    {
+        Running,
+        Success,
+        Failure
+    }
+
+    public abstract class Node
+    {
+        public NodeState state;
+        public Node parent;
+        public List<Node> childrens = new List<Node>();
+
+        private Dictionary<string, object> data = new Dictionary<string, object>();
+
+        public Node ()
+        {
+            parent = null;
+        }
+
+        public Node (List<Node> childrens)
+        {
+            foreach (Node children in childrens)
+            {
+                Attach(children);
+            }
+        }
+
+        public void Attach (Node node)
+        {
+            node.parent = this;
+        }
+
+        public virtual NodeState Evaluate () => NodeState.Failure;
+
+        public void SetData (string key, object value)
+        {
+            if (data.ContainsKey(key))
+                data[key] = value;
+            else
+                data.Add(key, value);
+        }
+
+        public object GetData<T> (string key)
+        {
+            if (data.TryGetValue(key, out var value))
+                return value;
+
+            Node parentNode = parent;
+            while (parentNode != null)
+            {
+                value = parentNode.GetData<T>(key);
+                if (value != null)
+                    return value;
+
+                parentNode = parent;
+            }
+
+            return default;
+        }
+
+        public bool RemoveData (string key)
+        {
+            if (data.ContainsKey(key))
+            {
+                data.Remove(key);
+                return true;
+            }
+
+            Node parentNode = parent;
+            while (parentNode != null)
+            {
+                bool cleaned = parentNode.RemoveData(key);
+                if (cleaned)
+                    return true;
+
+                parentNode = parent;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Simple IA/Assets/Scripts/NodeIntelligence/NodeRoot.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Intelligence
+{
+    public class NodeRoot : Node
+    {
+        public NodeRoot()
+        {
+        }
+
+        public NodeRoot(List<Node> childrens) : base(childrens)
+        {
+        }
+
+        public override NodeState Evaluate()
+        {
+            foreach (Node node in childrens)
+            {
+                switch (node.Evaluate())
+                {
+                    case NodeState.Running:
+                        state = NodeState.Running;
+                        break;
+                    case NodeState.Success:
+                        state = NodeState.Success;
+                        break;
+                    case NodeState.Failure:
+                        state = NodeState.Failure;
+                        break;
+                    default:
+                        Debug.Log("Entró al caso default. ERROR!");
+                        break;
+                }
+            }
+
+            return state;
+        }
+    }
+}
