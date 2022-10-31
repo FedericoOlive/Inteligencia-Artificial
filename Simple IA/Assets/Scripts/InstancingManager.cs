4d11e1b744a491b346e0f35291b9ea38e8a7d1d3
Sauron <federicoolive@gmail.com>
2022-10-30 23:17:23 -0300

    Update AI to tank

---
+++ Simple IA/Assets/Scripts/InstancingManager.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstancingManager : MonoBehaviour
+{
+    private List<PopulationManager> populationManagers = new List<PopulationManager>();
+
+    
+}
---
+++ Simple IA/Assets/Scripts/Mine.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    private Team team;
+
+
+    public void SetMine (Team newTeam)
+    {
+
+    }
+
+    
+}
